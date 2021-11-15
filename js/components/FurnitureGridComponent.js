60a2327df7e6cbb911f62f0a4d59991765a2805e
Gerardas Ciegis <gerardasc@gmail.com>
2021-11-15 13:15:43 +0200

    6: siuntimas komponenta

--- js/components/FurnitureGridComponent.js
+++ js/components/FurnitureGridComponent.js
@@ -1,10 +1,34 @@
 class FurnitureGridComponent {
   constructor () {
+    this.state = {
+      loading: false,
+      furnitures: []
+    }
     this.init();
   }
 
+  fetchFurnitures = () => API.fetchFurnitures(this.saveFurnitures, console.error)
+
+  saveFurnitures = (furnitures) => {
+    this.state.furnitures = furnitures;
+    this.state.loading = false;
+
+    this.render();
+  }
+
   init = () => {
+    this.state.loading = true;
+    this.fetchFurnitures();
     this.htmlElement = document.createElement('div');
-    this.htmlElement.innerHTML = 'Test'
+    this.render();
+  }
+
+  render = () => {
+    const { loading, furnitures } = this.state;
+    if (loading) {
+      this.htmlElement.innerHTML = 'siunčiama...';
+    } else {
+      this.htmlElement.innerHTML = 'parsiųsta';
+    }
   }
 }
